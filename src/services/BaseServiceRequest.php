0dbc90039bff30b1b0351dd2f5638b17a21b3ff5
Artem Smirnov <d3v3r73e@gmail.com>
2018-09-10 15:43:55 +0300

    Fork init

--- src/services/BaseServiceRequest.php
+++ src/services/BaseServiceRequest.php
@@ -3,14 +3,15 @@
 namespace Platron\Atol\services;
 
 abstract class BaseServiceRequest {
-    const REQUEST_URL = 'https://online.atol.ru/possystem/v3/';
-    
+//    const REQUEST_URL = 'https://online.atol.ru/possystem/v3/';
+    const REQUEST_URL = 'https://testonline.atol.ru/possystem/v3/';
+
     /**
 	 * Получить url ждя запроса
 	 * @return string
 	 */
 	abstract public function getRequestUrl();
-    
+
     /**
 	 * Получить параметры, сгенерированные командой
 	 * @return array
